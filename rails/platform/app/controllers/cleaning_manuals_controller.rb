a1d4acdc3fbb27d849ee21541d960c6f0578bcae
zomia <zomians@outlook.jp>
2026-03-15 17:17:54 +0900

    feat(platform): Web清掃マニュアルに業種別アクセス制御を追加 (issue#249)

--- rails/platform/app/controllers/cleaning_manuals_controller.rb
+++ rails/platform/app/controllers/cleaning_manuals_controller.rb
@@ -1,10 +1,11 @@
 class CleaningManualsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
+  before_action :set_client
+  before_action :require_feature!
+
   def index
-    @client_code = params[:client_code] || ""
-    @client = Client.find_by(code: @client_code)
-    @manuals = if @client_code.present?
+    @manuals = if @client
                  CleaningManual.for_client(@client_code).recent
                else
                  CleaningManual.none
@@ -12,18 +13,30 @@ class CleaningManualsController < ApplicationController
   end
 
   def show
-    @client_code = params[:client_code]
-    @client = Client.find_by(code: @client_code)
     @manual = CleaningManual.for_client(@client_code).find(params[:id])
   end
 
   def new
-    @client = Client.find_by(code: params[:client_code])
   end
 
   private
 
+  def set_client
+    @client_code = params[:client_code] || ""
+    @client = Client.find_by(code: @client_code)
+  end
+
+  def require_feature!
+    return if @client&.feature_available?(:cleaning_manuals)
+
+    if @client
+      redirect_to client_path(@client), alert: "この機能はご利用いただけません"
+    else
+      redirect_to clients_path, alert: "クライアントを選択してください"
+    end
+  end
+
   def record_not_found
-    redirect_to cleaning_manuals_path, alert: "マニュアルが見つかりません"
+    redirect_to cleaning_manuals_path(client_code: @client_code), alert: "マニュアルが見つかりません"
   end
 end
--- rails/platform/spec/requests/web/cleaning_manuals_spec.rb
+++ rails/platform/spec/requests/web/cleaning_manuals_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Web::CleaningManuals", type: :request do
   let(:user) { create(:user) }
-  let(:client) { create(:client, code: "test_client") }
+  let(:client) { create(:client, :hotel, code: "test_client") }
 
   describe "GET /cleaning_manuals" do
     context "未認証の場合" do
@@ -15,9 +15,20 @@ RSpec.describe "Web::CleaningManuals", type: :request do
     context "認証済みの場合" do
       before { sign_in user }
 
-      it "200を返すこと" do
-        get cleaning_manuals_path
+      it "hotelクライアントは200を返すこと" do
+        get cleaning_manuals_path(client_code: client.code)
         expect(response).to have_http_status(:ok)
+      end
+
+      it "非hotelクライアントはリダイレクトすること" do
+        restaurant_client = create(:client, code: "restaurant_test", industry: "restaurant")
+        get cleaning_manuals_path(client_code: restaurant_client.code)
+        expect(response).to redirect_to(client_path(restaurant_client))
+      end
+
+      it "クライアント未指定はクライアント一覧にリダイレクトすること" do
+        get cleaning_manuals_path
+        expect(response).to redirect_to(clients_path)
       end
     end
   end
@@ -25,7 +36,7 @@ RSpec.describe "Web::CleaningManuals", type: :request do
   describe "GET /cleaning_manuals/new" do
     context "未認証の場合" do
       it "ログイン画面にリダイレクトすること" do
-        get new_cleaning_manual_path
+        get new_cleaning_manual_path(client_code: client.code)
         expect(response).to redirect_to(new_user_session_path)
       end
     end
@@ -33,9 +44,15 @@ RSpec.describe "Web::CleaningManuals", type: :request do
     context "認証済みの場合" do
       before { sign_in user }
 
-      it "200を返すこと" do
-        get new_cleaning_manual_path
+      it "hotelクライアントは200を返すこと" do
+        get new_cleaning_manual_path(client_code: client.code)
         expect(response).to have_http_status(:ok)
+      end
+
+      it "非hotelクライアントはリダイレクトすること" do
+        restaurant_client = create(:client, code: "restaurant_new", industry: "restaurant")
+        get new_cleaning_manual_path(client_code: restaurant_client.code)
+        expect(response).to redirect_to(client_path(restaurant_client))
       end
     end
   end
@@ -53,9 +70,15 @@ RSpec.describe "Web::CleaningManuals", type: :request do
     context "認証済みの場合" do
       before { sign_in user }
 
-      it "200を返すこと" do
+      it "hotelクライアントは200を返すこと" do
         get cleaning_manual_path(manual, client_code: client.code)
         expect(response).to have_http_status(:ok)
+      end
+
+      it "非hotelクライアントはリダイレクトすること" do
+        restaurant_client = create(:client, code: "restaurant_show", industry: "restaurant")
+        get cleaning_manual_path(manual, client_code: restaurant_client.code)
+        expect(response).to redirect_to(client_path(restaurant_client))
       end
     end
   end
